3538c4888590c344bf81f359922e8b3c9b5df98b
ansinzhou <ansin0517@gmail.com>
2020-06-16 18:51:53 +0000

    ETL Pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,43 @@
 import sys
-
+import pandas as pd
+import numpy as np
+import sqlite3
+import re
+import sqlalchemy
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories, on=["id"])
+    return df
 
 def clean_data(df):
-    pass
-
+    df.drop('original', axis=1, inplace=True)
+    # create a dataframe of individual category columns
+    categories = df.categories.str.split(";", expand=True)
+    # select the first row of the caregories dataframe
+    row = categories.iloc[0]
+    #use row to extract a list of new column names for categories              
+    category_colnames = row.apply(lambda x: x[:-2])
+    categories.columns = category_colnames
+    # convert category values to just numbers 0 or 1
+    for column in categories:
+        categories[column] = categories[column].apply(lambda x: x[-1:])
+        categories[column] = categories[column].astype(int)
+    # replace categories column in df with new category columns   
+    df.drop('categories', axis = 1, inplace = True)
+    df = pd.concat([df, categories], axis=1)
+    # replace duplicates
+    df = df.drop_duplicates(keep = "first")
+   
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('df', engine, index=False)
+    
+    pass
 
 
 def main():
